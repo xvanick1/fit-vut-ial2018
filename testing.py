c36ed954b4a4d562d1e9f3eb30226786f6a93717
Pepino Adamek <pepino.adamek@gmail.com>
2018-10-20 02:10:33 +0200

    *feature* Added possibility to run interval of tests

--- testing.py
+++ testing.py
@@ -4,13 +4,14 @@
 Testing script of graph coloring program
 
 Use:
-$ python3 testing.py
+$ python3 testing.py [-a A] [-b B]
+	A ... first test
+	B ... last test
 
 Tests must be in "tests/" folder with format:
 - "testX.in" for input graph
 - "testX.out" for output (minimal chromatic number)
 
-TODO: Run tests only from X to Y for convenience
 '''
 
 import argparse
@@ -26,19 +27,23 @@ def arguments():
 
 	parser = argparse.ArgumentParser()
 
-	parser.add_argument('-m',
-		help="max tests to run (default is all tests)")
+	interval_args = parser.add_argument_group('arguments specifying testing interval')
+	interval_args.add_argument('-a',
+		help="beginning of interval (defaults to first test)")
+	interval_args.add_argument('-b',
+		help="end of interval (defaults to last test)")
 
 	return parser.parse_args()
 
-def execute_tests(num_of_tests):
+def execute_tests(a, b):
 
 	passed = 0
 	not_passed = []
 
-	''' Compare every program output with expected value '''
-	for x in range(1, num_of_tests + 1):
+	''' Run every test in given interval '''
+	for x in range(a, b + 1):
 
+		''' Get test filenames '''
 		test_in = "tests/test{}.in".format(x)
 		test_out = "tests/test{}.out".format(x)
 
@@ -47,7 +52,7 @@ def execute_tests(num_of_tests):
 			stdout=subprocess.PIPE).stdout.readline()
 		out = int(out)
 
-		''' Read test file '''
+		''' Read test output file '''
 		test_out_file = open(test_out, 'r')
 		expected = int(test_out_file.read())
 
@@ -72,30 +77,39 @@ def main():
 	''' Parsing arguments '''
 	args = arguments()
 
-	''' Get number of tests '''
+	''' Get number of tests from "tests" folder '''
 	test_files = 0
 	for line in subprocess.Popen(["ls", "tests", '-1'], # This is -one actually
 			stdout=subprocess.PIPE).stdout:
 		test_files += 1
 	num_of_tests = test_files // 2
 
-	''' If specified, only given number of tests will be run '''
-	if args.m:
-		max_tests = int(args.m)
-		if max_tests and max_tests <= num_of_tests:
-			num_of_tests = max_tests
+	''' Set default interval of tests (all) '''
+	a = 1
+	b = num_of_tests
 
-	''' Iterate through all tests ''' 
-	passed, not_passed = execute_tests(num_of_tests)
+	''' If specified, only given interval of tests will be run '''
+	if args.a:
+		if 0 < int(args.a) <= num_of_tests:
+			a = int(args.a)
+	if args.b:
+		if 0 < int(args.b) <= num_of_tests:
+			b = int(args.b)
+
+	''' Iterate through tests in interval < a ; b > ''' 
+	passed, not_passed = execute_tests(a, b)
 
 	''' Print final outcome '''
-	if passed == num_of_tests:
+	interval = (b - a) + 1 # number of executed tests
+	if passed == interval:
 		print("\n" + bcolors.OKGREEN + bcolors.BOLD 
-			+ "Passed {} out of {} tests".format(passed, num_of_tests)
+			+ "Passed {} out of {} tests ({} total)".
+			format(passed, interval, num_of_tests)
 			+ bcolors.ENDC + "\n")
 	else:
 		print("\n" + bcolors.FAIL + bcolors.BOLD 
-			+ "Passed {} out of {} tests".format(passed, num_of_tests)
+			+ "Passed {} out of {} tests ({} total)".
+			format(passed, interval, num_of_tests)
 			+ bcolors.ENDC)
 		print("Tests not passed: ", end = "")
 		for x in not_passed:
