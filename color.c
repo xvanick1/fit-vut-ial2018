c779686a0942f5dd443cc0a14f3c40c68d4c7fdc
Pepino Adamek <pepino.adamek@gmail.com>
2018-10-28 21:58:59 +0100

    *retraktoring* Changed indexing of colors and printing solution

--- color.c
+++ color.c
@@ -43,6 +43,7 @@ TODO:
 - finish documentation
 - error handling
 - in case of error, free all allocated memory
+- check if it runs on eva
 
 - maybe try forward-checking
 
@@ -65,6 +66,16 @@ typedef enum {
 	MINIMAL
 } MODE;
 
+/* Prints nodes info */
+void print_info() {
+
+	if(brief_flag) return;
+
+	printf("\nNUMBER OF NODES: %d\n", num_of_nodes);
+	printf("INFO: File with nodes loaded successfully. "
+		"Please wait for solution...\n\n");
+}
+
 /* Prints coloring of nodes */
 void print_coloring(int *min_colored_array, int min_chromatic_num, 
 	int mode) {
@@ -79,35 +90,65 @@ void print_coloring(int *min_colored_array, int min_chromatic_num,
 			printf("SMALLER CHROM. NUMBER {%d}: ", min_chromatic_num);
 
 		for (int i = 0; i < num_of_nodes; ++i) {
-			printf(" %d", node_array[i].color);
+			if(node_array[i].color == -1)
+				printf(" -");
+			else
+				printf(" %d", node_array[i].color);
 		}
 		printf("\n");
 	}
 
 	else if(mode == MINIMAL) {
 
-		printf("NUMBER OF NODES: %d\n\n", num_of_nodes);
+		// TODO: Drop the occasional newline in printed solution
+		// TODO: Change indexes to +1
 
-		printf("NODE ID\t|\tCOLOR\t|\tNEIGHBORS\t\t|\n");
-		printf("########|###############|###############################|\n");
+		printf("|###################|###################|###################|\n");
+		printf("%-20s%-20s%-20s|\n","| NODE ID", "| COLOR", "| NEIGHBORS");
+		printf("|###################|###################|###################|\n");
+
+		/* Calculate max decimal places one neighbor will take */
+		int num = num_of_nodes;
+		int max_dec_places = 1;
+		while(num > 10) {
+			num /= 10;
+			max_dec_places++;
+		}
+
+		/* Max neighbors that will fit the line */
+		int max_neighbors = 10 / max_dec_places;
+
 		for(int i = 0; i < num_of_nodes; i++) {
-			printf("%d\t|\t%d\t|\t", i, min_colored_array[i]-1);
-			int neighbors = 0;
+			
+			printf("| %-18d| %-18d|", i, min_colored_array[i]);
+
+			int lenght = 0;
+			char neighbor[20];
+
+			int count = 0;
 			for (int j = 0; j < num_of_nodes; j++)
 			{
 				if(graph_table[i][j] == true) {
-					if(neighbors != 0 && neighbors%10 == 0) {
-						printf("\t|");
-						printf("\n\t|\t\t|\t");
+					printf(" %d", j);
+					sprintf(neighbor, " %d", j);
+					lenght += strlen(neighbor);
+
+					if(count >= max_neighbors) {
+						printf("%*s|", 19 - lenght, "");
+						printf("\n|%19s|%19s|", "", "");
+						count = 0;
+						lenght = 0;
 					}
-					neighbors++;
-					printf("%d ", j);
+					else 
+						count++;
+
 				}
 			}
-			printf("\n########|###############|###############################|\n");
+			printf("%*s|", 19 - lenght, "");
+			printf("\n|###################|###################|###################|\n");
 		}
 
-		printf("\nMINIMAL CHROMATIC NUMBER: %d\n", min_chromatic_num);
+		printf("\nMINIMAL CHROMATIC NUMBER: %d\n", min_chromatic_num + 1);
 	}
 
 	else {
@@ -140,7 +181,7 @@ void success(NodeStack *stack, int *min_colored_array, int num_of_nodes,
 	int *min_chromatic_num) {
 
 	/* Getting max color from node array */
-	int max_color = 0; 
+	int max_color = -1; 
 	for (int i = 0; i < num_of_nodes; i++) {
 		if(node_array[i].color > max_color) {
 			max_color = node_array[i].color;
@@ -148,7 +189,7 @@ void success(NodeStack *stack, int *min_colored_array, int num_of_nodes,
 	}
 	
 	/* Making sure I save color array only with 
-	smaller chromatic number */
+	smaller chromatic number than I already have */
 	if(max_color < *min_chromatic_num) {
 
 		*min_chromatic_num = max_color;
@@ -162,7 +203,7 @@ void success(NodeStack *stack, int *min_colored_array, int num_of_nodes,
 		/* OPTIMIZATION #2
 		Find the first node from left in min_colored_array, that has color
 		same as min_chromatic_num and pop it and all neighbors next to its
-		right, because this part of tree is useless */
+		right, because this part of tree is useless for our cause */
 		int useless = -1;
 		for (int i = 0; i < num_of_nodes; i++)
 			if(min_colored_array[i] == *min_chromatic_num) {
@@ -170,17 +211,20 @@ void success(NodeStack *stack, int *min_colored_array, int num_of_nodes,
 				break;
 			}
 
+		/* Also colors of those nodes needs to be set to -1,
+		because they won't get available_color = -1 in get_color() */
 		if(useless != -1)
+
+			/* Pop useless nodes and cancel their colors */
 			while(1) {
 				int top_id = stack_top(stack)->id;
 				if(top_id != useless) {
-					node_array[top_id].color = 0;
+					node_array[top_id].color = -1;
 					stack_pop(stack);
 				}
+				/* Leftmost useless node was found */
 				else {
-					/* If I'm not zeroing color here,
-					solution is different */
-					node_array[top_id].color = 0;
+					node_array[top_id].color = -1;
 					stack_pop(stack);
 					break;
 				}
@@ -193,7 +237,7 @@ int get_color(bool* colors, Node* node, int num_of_nodes,
 	int min_chromatic_num) {
 	
 	/* Initialize all colors as available */
-	for(int i = 1; i < num_of_nodes + 1; i++) {
+	for(int i = 0; i < num_of_nodes; i++) {
 		colors[i] = true;
 	}
 
@@ -204,19 +248,21 @@ int get_color(bool* colors, Node* node, int num_of_nodes,
 		/* Neighbor was found */
 		if(graph_table[node->id][i] == true) {
 
-			/* Set color of neighbor as unavailable, even when 
-			color is 0 (none) */
-			colors[node_array[i].color] = false;
+			/* Set color of neighbor as unavailable */
+			int neighbor_color = node_array[i].color;
+			if(neighbor_color != -1)
+				colors[neighbor_color] = false;
+
 		}
 	}
 
 	/* Choosing the lowest available color */
-	int available_color = 0;
+	int available_color = -1;
 	/* Minimal color must be node's last color + 1, so that the node
 	iterates through all its possible colors */
 	/* OPTIMIZATION #1
 	Also it shouldn't get color >= than saved minimal chromatic
-	number, since I need only smaller solutions */
+	number, since we need only smaller solutions */
 	for(int i = node->color + 1; i < min_chromatic_num; i++) {
 		if(colors[i] == true) {
 			available_color = i;
@@ -224,20 +270,25 @@ int get_color(bool* colors, Node* node, int num_of_nodes,
 		}
 	}
 
+
 	/* available_color == 0 means this node won't get any color */
 	return available_color;
 }
 
 void backtracking_csp(NodeStack *stack, int num_of_nodes) {
 
-	/* min_chromatic_num must be bigger by 1 than num_of_nodes, so
+	/* min_chromatic_num must be at start equal to num_of_nodes, so
 	graphs with property colors == nodes get solution saved to array */
-	int min_chromatic_num = num_of_nodes + 1;
+	int min_chromatic_num = num_of_nodes;
 	int *min_colored_array = calloc(num_of_nodes, sizeof(*min_colored_array));
+	
+	for(int i = 0; i < num_of_nodes; i++) {
+		min_colored_array[i] = -1;
+	}
 
 	/* For saving colors of neighbors and then finding lowest 
 	available */ 
-	bool *colors = calloc(num_of_nodes + 1, sizeof(*colors));
+	bool *colors = calloc(num_of_nodes, sizeof(*colors));
 
 	/* Pushing first node to stack */
 	stack_push(stack, &(node_array[0]));
@@ -260,7 +311,7 @@ void backtracking_csp(NodeStack *stack, int num_of_nodes) {
 			// print_coloring(min_colored_array, min_chromatic_num, NORMAL);	
 
 			/* Node can be colored (rule can be applied) */
-			if(node->color != 0) {
+			if(node->color != -1) {
 
 				/* One of goals was reached (last node in array
 				was colored) */
@@ -287,11 +338,12 @@ void backtracking_csp(NodeStack *stack, int num_of_nodes) {
 		}
 	}
 
-	/* Print minimal solution */
+	/* Print minimal solution. Since indexing of nodes and colors
+	is from 0, to printed solution is added 1 for better comprehension */
 	if(!brief_flag)
 		print_coloring(min_colored_array, min_chromatic_num, MINIMAL);
 	else
-		printf("%d\n", min_chromatic_num);
+		printf("%d\n", min_chromatic_num + 1);
 
 	free(min_colored_array);
 	free(colors);
@@ -302,7 +354,7 @@ void fill_node(FILE* file, Node *node, int node_id,
 
 	/* Getting node id and initializing it */
 	node->id = node_id;
-	node->color = 0;
+	node->color = -1;
 
 	char c;
 	int position = 0;
@@ -348,13 +400,17 @@ void create_graph(char* filename) {
     file = fopen(filename,"r");
     if(file == NULL) {
         fprintf(stderr, "ERROR: File opening failed\n");
-        exit(-1);
+        exit(1);
     }
 
     /* Getting number of nodes */
     char count[100];
-    fgets(count, 100, file); // reads 100 chars or line
+    fgets(count, 100, file); // reads 100 chars or until newline
     sscanf(count, "%d\n", &num_of_nodes);
+    if(num_of_nodes <= 0) {
+    	fprintf(stderr, "ERROR: Number of nodes is not valid\n");
+        exit(-1);
+    } 
 
     /* Node array where nodes are expected to be organized by id 
     for simple lookup by index of array */
@@ -376,6 +432,9 @@ void create_graph(char* filename) {
     check_matrix();
     
     fclose(file); 
+
+    /* Print info */
+    print_info();
 
     return;
 }
@@ -460,7 +519,7 @@ int main(int argc, char* argv[]) {
     /* Apply coloring algorithm on nodes and print solution */
     backtracking_csp(&stack, num_of_nodes);
 
-    /* Freeing memory */
+    /* Free memory */
     for(int i = 0; i < num_of_nodes; i++) {
     	free(graph_table[i]);
     }
